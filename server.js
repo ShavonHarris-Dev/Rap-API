4425933455c1e10b8d0e584b50ccf750ab6d3418
Shavon Harris <Shavonharris9114@gmail.com>
2022-02-21 21:53:35 +0000

    added port

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get('/api/rappers/:rapperName', (request, response) => {
     
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
